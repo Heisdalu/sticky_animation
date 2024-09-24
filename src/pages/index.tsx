80600ab82cf7c8e1f17651d8ba20f10769a5d499
divine <divineobi07@gmail.com>
2024-09-24 14:40:24 +0100

    reset

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,10 @@
-import ItemCard from "@/components/ItemCard";
+import Lol from "@/components/Scrolling/main";
 
-const colors = [
-  { name: "Teal", hex: "#008080" },
-  { name: "Coral", hex: "#FF7F50" },
-  { name: "Royal Blue", hex: "#4169E1" },
-  { name: "Goldenrod", hex: "#DAA520" },
-  { name: "Crimson", hex: "#DC143C" },
-  { name: "Forest Green", hex: "#228B22" },
-
-  //framer debug...
-  //layout animation....nexted modals
-  // animated nav tab with width animations
-];
-const Lol = () => {
+const Index = () => {
   return (
-    <div className="bg-black">
-      <div className="">
-        <div className="border-[3px] border-black h-[100vh] text-white text-[2rem] grid place-items-center">
-          Scroll Down
-        </div>
-        {colors.map((el, i) => (
-          <ItemCard key={el.name} obj={{ ...el, id: i }} />
-        ))}
-      </div>
-      <div className="border-[3px] border-black h-[100vh] text-white text-[2rem] grid place-items-center">
-        Scroll Up
-      </div>
+    <div>
+      <Lol />
     </div>
   );
 };
-export default Lol;
+export default Index;
